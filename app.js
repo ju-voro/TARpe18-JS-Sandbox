da5eb9a8126d7f94eff5e53066de6632b77bbae4
Julia Aasmaa <my_e_10@hotmail.com>
2019-12-09 10:04:37 +0200

    commit

--- app.js
+++ app.js
@@ -24,7 +24,31 @@ let html = `
         <li>${name} says: ${hello()}</li>
     </ul>
 `;
-
-
-
 document.body.innerHTML = html;
+
+//get the data type of John's job
+console.log(typeof(job));
+//the data type of John's age
+console.log(typeof(age));
+
+//arrays
+const numbers1 = [1, 2, 3, 4, 5];
+console.log(numbers1);
+console.log(typeof(numbers1));
+
+const numbers2 = new Array(6, 7, 8, 9, 10);
+//foreach loop
+//arrow function
+numbers2.forEach(number => {
+    console.log("Let count: ", number);
+});
+
+const mixedArray = [22, "banana", true];
+mixedArray.forEach(element => {
+    console.log(element, " is a ", typeof(element));
+});
+
+//the length of the string element
+console.log("Banana is a ", mixedArray[1].length, " chars long");
+
+//add elements to an array
